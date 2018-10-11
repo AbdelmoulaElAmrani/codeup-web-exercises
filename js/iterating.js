35d98abb36db34f16004765292bebdae51f1ad4f
AbdelmoulaElAmrani <elamraniabdelmoula@gmail.com>
2018-10-11 16:21:26 -0500

    add arrays and object exercises

---
+++ js/iterating.js
@@ -0,0 +1,94 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+var names = [ "dara" , "slopy" , "pika" , "soso"];
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+console.log(names.length);
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+for (var i = 0 ; i <names.length ; i++) {
+    console.log(names[i])
+}
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+names.forEach(function (name) {
+    console.log(name);
+});
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+// names.forEach(function (value, index, array) {
+//     console.log(index);
+// })
+    function first(value) {
+        console.log(names[0]);
+    }
+    function second(value) {
+        console.log(names[1]);
+    }
+    function last(value) {
+        console.log(names[3]);
+    }
+    names.forEach(last);
+
+})();
+
+// ---- bonus
+
+// 1. Create a function that returns a random day of the week
+var randomDaycalc;
+var daysOfWeek;
+function randomDay() {
+    daysOfWeek =  ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday" , "sunday"];
+    randomDaycalc = Math.round(Math.random() * (daysOfWeek.length - 1));
+
+}
+
+console.log(randomDay(daysOfWeek
+
+));
+
+
+// 2. Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
+// someFunction("a") // returns 1
+// someFunction("z") // returns 26
+
+// 3. Create a function that returns the longest string in a given array of string elements
+// 4. Create a function that takes in two arrays of number inputs. If all numbers added together in the first array is equal to all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
+//     var arr1 = ['bob', 1, true, 1, 2];
+// var arr2 = [2, null, undefined, 0, 2, "apple"]
+// exampleFunction(arr1, arr2) // returns true
